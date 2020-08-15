f99654567f02afbf4522bc5f7882f83409413eff
skorp-dev <skorpwebx@gmail.com>
2020-08-16 00:20:57 +0200

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 $(function(){
 
-    var mixer = mixitup('.products__inner-box');
+    
 
-    $(".rate-star").rateYo({
+    $('.rate-star').rateYo({
         rating: 4.5,
         starWidth: "12px",
         readOnly: true
@@ -14,4 +14,29 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4
     });  
+
+    $('.js-range-slider').ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+
+    });
+
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
 });
