e6602d021a58f8dcdf14a798a8491ddccaf2b774
skorp-dev <skorpwebx@gmail.com>
2020-08-21 03:59:10 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,32 @@ $(function(){
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1441,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 801,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });  
 
     $('.js-range-slider').ionRangeSlider({
@@ -45,6 +70,8 @@ $(function(){
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
         });
+
+    $('input[type="file"], select').styler();    
 
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
